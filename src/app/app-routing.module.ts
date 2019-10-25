ab0d56c3179e16237ea128256232edb58e25f467
FlorinTeglas <teglas_floryn07@yahoo.com>
2019-10-25 13:39:24 +0300

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductFormComponent} from './components/product-form/product-form.component';
+import {ListUserComponent} from './components/list-user/list-user.component';
+import {UserFormComponent} from './components/user-form/user-form.component';
+import {LoginComponent} from './components/login/login.component';
+import {AdminComponent} from './components/admin/admin.component';
+import {AuthGuard} from './auth.guard';
+import {ListProductsComponent} from './components/list-products/list-products.component';
+import {ClientProductsComponent} from './components/client-products/client-products.component';
+import {ListCartComponent} from './components/list-cart/list-cart.component';
+import {ListCommComponent} from './components/list-comm/list-comm.component';
+import {AuthClientGuard} from './auth-client.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin/products', component: ListProductsComponent, canActivate: [AuthGuard]},
+  {path: 'admin/products/op', component: ProductFormComponent, canActivate: [AuthGuard]},
+  {path: 'admin/users', component: ListUserComponent, canActivate: [AuthGuard]},
+  {path: 'admin/users/op', component: UserFormComponent, canActivate: [AuthGuard]},
+  {path: '', component: LoginComponent},
+  {path: 'admin', component: AdminComponent, canActivate: [AuthGuard]},
+  {path: 'client', component: ClientProductsComponent, canActivate: [AuthClientGuard]},
+  {path: 'client/orders', component: ListCartComponent, canActivate: [AuthClientGuard]},
+  {path: 'client/comments', component: ListCommComponent, canActivate: [AuthClientGuard]}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ProductService} from './shared_service/product.service';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserService } from './shared_service/user.service';
+import { LoginComponent } from './components/login/login.component';
+import { LoginService } from './shared_service/login.service';
+import { AuthGuard } from './auth.guard';
+import { AdminComponent } from './components/admin/admin.component';
+import { ClientProductsComponent } from './components/client-products/client-products.component';
+import {CartService} from './shared_service/cart.service';
+import { ListCartComponent } from './components/list-cart/list-cart.component';
+import { ListCommComponent } from './components/list-comm/list-comm.component';
+import {CommService} from './shared_service/comm.service';
+import {AuthClientGuard} from './auth-client.guard';
+
+const appRoutes: Routes = [
+  {path: 'admin/products/op', component: ProductFormComponent},
+  {path: 'admin/users', component: ListUserComponent},
+  {path: 'admin/users/op', component: UserFormComponent},
+  {path: '', component: LoginComponent},
+  {path: 'admin', component: AdminComponent, canActivate: [AuthGuard],
+    children: [{path: 'products', component: ListProductsComponent}]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductsComponent,
+    ProductFormComponent,
+    ListUserComponent,
+    UserFormComponent,
+    LoginComponent,
+    AdminComponent,
+    ClientProductsComponent,
+    ListCartComponent,
+    ListCommComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    // RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CartService, ProductService, UserService, LoginService, AuthGuard, CommService, AuthClientGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
